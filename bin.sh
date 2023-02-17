55d34ee6c584e60ced6fa90f2abcc114c69eafd2
joby62 <125260326+joby62@users.noreply.github.com>
2023-02-17 16:48:03 +0800

    Update bin.sh to use conda smoothly.

--- bin.sh
+++ bin.sh
@@ -11,7 +11,11 @@ echo "全局安装pm2"
 sudo npm install -g pm2@5.2.2
 
 echo "为pm2安装个日志管理"
-pm2 install pm2-logrotate@2.7.0
+pm2 install pm2-logrotatd@2.7.0
+
+echo "下载安装mysql客户端和服务"
+sudo apt install mysql-server
+sudo apt install mysql-client
 
 echo "下载miniconda"
 wget https://repo.anaconda.com/miniconda/Miniconda3-py38_4.12.0-Linux-x86_64.sh
@@ -20,7 +24,20 @@ echo "安装miniconda, 记得最后init时选择yes"
 sh Miniconda3-py38_4.12.0-Linux-x86_64.sh
 
 echo "生效base的conda环境"
-source ~/.bashrc
+__conda_setup="$('/home/ubuntu/miniconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/home/ubuntu/miniconda3/etc/profile.d/conda.sh" ]; then
+        . "/home/ubuntu/miniconda3/etc/profile.d/conda.sh"
+    else
+        export PATH="/home/ubuntu/miniconda3/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+
+echo "使用conda创建python虚拟环境alpha_admin"
+conda create -n alpha_admin python==3.8.5
 
 echo "生效平台所需conda环境"
 conda activate alpha_admin
@@ -34,10 +51,6 @@ pip uninstall numpy
 echo "下载安装requirements.txt"
 wget -N --no-check-certificate https://raw.githubusercontent.com/joby62/ubuntu_quant/main/requirements.txt
 pip install -r requirements.txt -i https://mirrors.bfsu.edu.cn/pypi/web/simple
-
-echo "下载安装mysql客户端和服务"
-sudo apt install mysql-server
-sudo apt install mysql-client
 
 echo "删除本地无用文件"
 rm -r Miniconda3-py38_4.12.0-Linux-x86_64.sh
